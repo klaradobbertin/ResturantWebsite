589ee1325b8fa9158c6927a7e20d76d03cdca334
klaradobbertin <klara.dobbertin@hotmail.com>
2024-03-02 23:49:14 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Select the element with id "dryckSection"
+const dryckSection = document.getElementById("dryckSection");
+
+// Check if the element is found
+
+if (dryckSection) {
+    // Change the background image
+    dryckSection.style.backgroundImage = "url('drink.jpg')";
+}
